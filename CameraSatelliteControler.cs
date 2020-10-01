159c44b41feed1580baf3c8cdcd5d06d73d3c6d1
tlegorju <thibault.legorju@gmail.com>
2020-10-01 15:30:58 +0200

    exo2

---
+++ CameraSatelliteControler.cs
@@ -0,0 +1,218 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSatelliteControler : MonoBehaviour
+{
+    [SerializeField] Transform cameraTarget;
+    [SerializeField] Transform satellitCamera;
+
+    [SerializeField] private float startRho, startTheta, startPhi;
+
+    [SerializeField]
+    private float currentRho = 0, currentTheta = 0, currentPhi = 0;
+    private float CurrentRho
+    {
+        get
+        {
+            return Mathf.Lerp(minRho, maxRho, currentRho);
+        }
+        set
+        {
+            currentRho = value;
+        }
+    }
+    private float CurrentTheta
+    {
+        get
+        {
+            float tmp = currentTheta;
+            if (currentTheta > 1)
+                tmp = currentTheta - (int)currentTheta;
+            else if(currentTheta < 0)
+                tmp = 1+( currentTheta - (int)currentTheta);
+            Debug.Log("TMP : " + tmp);
+            return Mathf.Lerp(minTheta, maxTheta, tmp);
+        }
+        set
+        {
+            currentTheta = value;
+        }
+    }
+    private float CurrentPhi { 
+        get { 
+            return Mathf.Lerp(minPhi, maxPhi, currentPhi); 
+        }
+        set
+        {
+            currentPhi = value;
+            minRho = minRhoCurve.Evaluate(currentPhi);
+            maxRho = maxRhoCurve.Evaluate(currentPhi);
+        }
+    }
+    [SerializeField]
+    private float targetRho, targetTheta, targetPhi;
+    
+    [SerializeField]
+    private float minRho=1, maxRho=12, minTheta, maxTheta, minPhi, maxPhi;
+    [SerializeField]
+    private int numberOfRhoLevel = 12;
+    private int currentRhoLevel = 1;
+    [SerializeField]
+    private int numberOfThetaLevel = 12;
+    private int currentThetaLevel = 1;
+
+    [SerializeField]
+    private float speedMouseX = 150, speedMouseY = 150;
+
+    private Coroutine lerpingCoroutine=null;
+    [SerializeField] private float lerpingTime=1;
+
+    [SerializeField] private AnimationCurve minRhoCurve;
+    [SerializeField] private AnimationCurve maxRhoCurve;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ReinitializePosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            ReinitializePosition();
+        else if (Input.GetKeyDown(KeyCode.LeftControl))
+            MoveToPreviousThetaLevel();
+        else if (Input.GetKeyDown(KeyCode.LeftAlt))
+            MoveToNextThetaLevel();
+        else if(Input.mouseScrollDelta.y<-.1f)
+            MoveToNextRhoLevel();
+        else if (Input.mouseScrollDelta.y > .1f)
+            MoveToPreviousRhoLevel();
+
+        if (Input.GetMouseButton(1))
+        {
+            if (Mathf.Abs(Input.GetAxis("Mouse Y")) > .1f)
+            {
+                targetPhi = Mathf.Clamp01(targetPhi - Input.GetAxis("Mouse Y") * Time.deltaTime);
+            }
+            if (Mathf.Abs(Input.GetAxis("Mouse X")) > .1f)
+            {
+                targetTheta = targetTheta - Input.GetAxis("Mouse X") * Time.deltaTime;
+            }
+        }
+
+        CurrentRho = Mathf.MoveTowards(currentRho, targetRho, Time.deltaTime) ;
+        CurrentTheta = Mathf.MoveTowards(currentTheta, targetTheta, Time.deltaTime);
+        CurrentPhi = Mathf.MoveTowards(currentPhi, targetPhi, Time.deltaTime);
+    }
+
+    private void LateUpdate()
+    {
+        if(cameraTarget ==null)
+        {
+            Debug.LogError("Camera target null");
+            return;
+        }
+
+        satellitCamera.position = Vector3.Lerp(satellitCamera.position,
+            SphericalToCartesianPos(CurrentPhi, CurrentTheta, CurrentRho), Time.deltaTime);
+
+        satellitCamera.LookAt(cameraTarget);
+    }
+
+    private void ReinitializePosition()
+    {
+        targetRho = startRho;
+        targetTheta = startTheta;
+        targetPhi = startPhi;
+    }
+
+    private void MoveToNextRhoLevel()
+    {
+        targetRho = Mathf.Clamp01(targetRho + 1.0f / numberOfRhoLevel);
+    }
+
+    private void MoveToPreviousRhoLevel()
+    {
+        targetRho = Mathf.Clamp01(targetRho - 1.0f / numberOfRhoLevel);
+    }
+
+    private void MoveToNextThetaLevel()
+    {
+        int closestSuperiorLevel = (int)(targetTheta * numberOfThetaLevel +1);
+        //if(targetTheta<0)
+        //{
+        //    closestSuperiorLevel = (int)(targetTheta * numberOfThetaLevel);
+        //}
+
+        if(targetTheta>=0)
+        {
+            if ((((float)closestSuperiorLevel) / numberOfThetaLevel) - targetTheta >= 0.03f)
+            {
+                targetTheta = ((float)closestSuperiorLevel) / numberOfThetaLevel;
+            }
+            else
+            {
+                targetTheta = (float)(closestSuperiorLevel + 1) / numberOfThetaLevel;
+            }
+        }
+        else
+        {
+            if (Mathf.Abs((((float)closestSuperiorLevel) / numberOfThetaLevel) - targetTheta) >= 0.03f)
+            {
+                targetTheta = ((float)closestSuperiorLevel) / numberOfThetaLevel;
+            }
+            else
+            {
+                targetTheta = (float)(closestSuperiorLevel - 1) / numberOfThetaLevel;
+            }
+        }
+       
+        
+    }
+
+    private void MoveToPreviousThetaLevel()
+    {
+        int closestInferiorLevel = (int)(targetTheta * numberOfThetaLevel);
+        if (targetTheta < 0)
+        {
+            closestInferiorLevel = (int)((targetTheta-1) * numberOfThetaLevel);
+        }
+
+        if(targetTheta>=0)
+        {
+
+            if (targetTheta - ((float)closestInferiorLevel / numberOfThetaLevel) >= 0.03f)
+                targetTheta = (float)closestInferiorLevel / numberOfThetaLevel;
+            else if (targetTheta >= 0)
+                targetTheta = (float)(closestInferiorLevel - 1) / numberOfThetaLevel;
+        }
+        else
+        {
+            if (Mathf.Abs(targetTheta - ((float)closestInferiorLevel / numberOfThetaLevel)) >= 0.03f)
+                targetTheta = (float)(closestInferiorLevel+1) / numberOfThetaLevel;
+            else
+            {
+                targetTheta = (float)(closestInferiorLevel) / numberOfThetaLevel;
+            }
+        }
+    }
+
+    private Vector3 SphericalToCartesianPos(float phi, float theta, float rho)
+    {
+        Vector3 cartesianPos;
+        float radPhi = phi * Mathf.Deg2Rad;
+        float radTheta = theta * Mathf.Deg2Rad;
+
+        float a = rho * Mathf.Cos(radPhi); //??
+
+        cartesianPos.x = a * Mathf.Cos(radTheta);
+        cartesianPos.y = rho * Mathf.Sin(radPhi);
+        cartesianPos.z = a * Mathf.Sin(radTheta);
+
+        return cartesianPos;
+    }
+}
--- TP_Prog_2020-09-24/Assets/CameraSatelliteControler.cs
+++ TP_Prog_2020-09-24/Assets/CameraSatelliteControler.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 
 public class CameraSatelliteControler : MonoBehaviour
 {
-    [SerializeField] Transform cameraTarget;
-    [SerializeField] Transform satellitCamera;
+    [SerializeField] Transform cameraTarget;    //The object we want to look at
+    [SerializeField] Transform satellitCamera;  //The camera we want to move around
 
-    [SerializeField] private float startRho, startTheta, startPhi;
+    [SerializeField] private float startRho, startTheta, startPhi;  //The position we want to start at
 
-    [SerializeField]
-    private float currentRho = 0, currentTheta = 0, currentPhi = 0;
+    private float currentRho = 0, currentTheta = 0, currentPhi = 0; //The current position of the camera
     private float CurrentRho
     {
         get
         {
-            return Mathf.Lerp(minRho, maxRho, currentRho);
+            return currentRho;
         }
         set
         {
@@ -26,7 +25,7 @@ public class CameraSatelliteControler : MonoBehaviour
     {
         get
         {
-            return Mathf.Lerp(minTheta, maxTheta, currentTheta);
+            return currentTheta;
         }
         set
         {
@@ -34,34 +33,44 @@ public class CameraSatelliteControler : MonoBehaviour
         }
     }
     private float CurrentPhi { 
-        get { 
-            return Mathf.Lerp(minPhi, maxPhi, currentPhi); 
+        get {
+            return currentPhi;
         }
         set
         {
-            currentPhi = value;
-            minRho = minRhoCurve.Evaluate(Mathf.Lerp(minPhi, maxPhi, currentPhi));
-            maxRho = maxRhoCurve.Evaluate(Mathf.Lerp(minPhi, maxPhi, currentPhi));
+            currentPhi = value;                                //We change the bound of rho when we change phi
+            minRho = minRhoCurve.Evaluate(currentPhi);
+            maxRho = maxRhoCurve.Evaluate(currentPhi);
         }
     }
-    [SerializeField]
-    private float targetRho, targetTheta, targetPhi;
+
+    private float targetRho, targetTheta, targetPhi;            //Used for interpolating the position
+    private float TargetTheta { get { return targetTheta; } 
+        set {
+            targetTheta = value;    //If we did a full degree turn, we make sure the values remain between 0 & 1 while keeping the same rotation 
+            if(targetTheta<0)
+            {
+                targetTheta += 1;
+                currentTheta += 1;
+            }
+            else if(targetTheta>=1)
+            {
+                targetTheta -= 1;
+                currentTheta -= 1;
+            }
+        } }
     
     [SerializeField]
     private float minRho=1, maxRho=12, minTheta, maxTheta, minPhi, maxPhi;
-    [SerializeField]
-    private float numberOfRhoLevel = 12;
-    private float currentRhoLevel = 1;
-    [SerializeField]
-    private float numberOfThetaLevel = 12;
-    private float currentThetaLevel = 1;
 
     [SerializeField]
-    private float speedMouseX = 150, speedMouseY = 150;
+    private int numberOfRhoLevel = 12;
+    [SerializeField]
+    private int numberOfThetaLevel = 12;
 
-    private Coroutine lerpingCoroutine=null;
-    [SerializeField] private float lerpingTime=1;
-
+    [SerializeField]
+    private float speedMouseX = 15, speedMouseY = 15;
+    
     [SerializeField] private AnimationCurve minRhoCurve;
     [SerializeField] private AnimationCurve maxRhoCurve;
 
@@ -75,137 +84,130 @@ public class CameraSatelliteControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //    ReinitializePosition();
-        //else if (Input.GetKeyDown(KeyCode.LeftControl))
-        //    MoveToPreviousThetaLevel();
-        //else if (Input.GetKeyDown(KeyCode.LeftAlt))
-        //    MoveToNextThetaLevel();
-        //else if(Input.mouseScrollDelta.y<-.1f)
-        //    MoveToNextRhoLevel();
-        //else if (Input.mouseScrollDelta.y > .1f)
-        //    MoveToPreviousRhoLevel();
-        //else if(Input.GetMouseButton(1))
+        if (Input.GetKeyDown(KeyCode.Space))
+            ReinitializePosition();
+
+        else if (Input.GetKeyDown(KeyCode.LeftControl))
+            MoveToPreviousThetaLevel();
+        else if (Input.GetKeyDown(KeyCode.LeftAlt))
+            MoveToNextThetaLevel();
+
+        else if(Input.mouseScrollDelta.y<-.1f)
+            MoveToNextRhoLevel();
+        else if (Input.mouseScrollDelta.y > .1f)
+            MoveToPreviousRhoLevel();
+
         if (Input.GetMouseButton(1))
         {
-            if(Mathf.Abs(Input.GetAxis("Mouse Y"))>.1f)
+            if (Mathf.Abs(Input.GetAxis("Mouse Y")) > .1f)
             {
-                targetPhi = Mathf.Clamp01(targetPhi - Input.GetAxis("Mouse Y") * speedMouseY * Time.deltaTime);
+                targetPhi = Mathf.Clamp01(targetPhi - Input.GetAxis("Mouse Y") * Time.deltaTime * speedMouseY);
             }
             if (Mathf.Abs(Input.GetAxis("Mouse X")) > .1f)
             {
-                targetTheta = targetTheta - Input.GetAxis("Mouse X") * speedMouseX * Time.deltaTime;
-                if (targetTheta < 0)
-                    targetTheta += 1;
-                else if (targetTheta >= 1)
-                    targetTheta -= 1;
+                TargetTheta = TargetTheta - Input.GetAxis("Mouse X") * Time.deltaTime * speedMouseX;
             }
-            //StartLerpCoroutine();
         }
 
-        CurrentRho = Mathf.Lerp(currentRho, targetRho, Time.deltaTime) ;
-        CurrentTheta = Mathf.Lerp(currentTheta, targetTheta, Time.deltaTime);
-        CurrentPhi = Mathf.Lerp(currentPhi, targetPhi, Time.deltaTime);
+        CurrentRho = Mathf.MoveTowards(CurrentRho, targetRho, Time.deltaTime) ;
+        CurrentTheta = Mathf.MoveTowards(CurrentTheta, targetTheta, Time.deltaTime);
+        CurrentPhi = Mathf.MoveTowards(CurrentPhi, targetPhi, Time.deltaTime);
     }
 
     private void LateUpdate()
     {
-        if(cameraTarget ==null)
+        if(cameraTarget ==null || satellitCamera==null)
         {
-            Debug.LogError("Camera target null");
+            Debug.LogError("Camera target or satellit camera null");
             return;
         }
 
         satellitCamera.position = Vector3.Lerp(satellitCamera.position,
-            SphericalToCartesianPos(CurrentPhi, CurrentTheta, CurrentRho), Time.deltaTime);
+            SphericalToCartesianPos(GetInterpolatePhi(), GetInterpolateTheta(), GetInterpolateRho()), Time.deltaTime);
 
         satellitCamera.LookAt(cameraTarget);
     }
 
+    //Make the camera go back to the start position
     private void ReinitializePosition()
     {
         targetRho = startRho;
-        targetTheta = startTheta;
+        TargetTheta = startTheta;
         targetPhi = startPhi;
-
-
-
-        //StartLerpCoroutine();
     }
 
+    //Unzoom the camera
     private void MoveToNextRhoLevel()
     {
-        currentRhoLevel = Mathf.Min((currentRhoLevel + 1),numberOfRhoLevel);
-        targetRho = Mathf.Lerp(minRho, maxRho, currentRhoLevel / numberOfRhoLevel);
-
-        //StartLerpCoroutine();
+        targetRho = Mathf.Clamp01(targetRho + 1.0f / numberOfRhoLevel); //Rho is only modified by the mouse wheel, so the targetRho is always on a valid step
     }
 
+    //Zoom the camera
     private void MoveToPreviousRhoLevel()
     {
-        currentRhoLevel = Mathf.Max(currentRhoLevel-1,0);
-        targetRho = Mathf.Lerp(minRho, maxRho, currentRhoLevel / numberOfRhoLevel);
-
-        //StartLerpCoroutine();
+        targetRho = Mathf.Clamp01(targetRho - 1.0f / numberOfRhoLevel);
     }
 
+    //Turn the camera counter clockwise
     private void MoveToNextThetaLevel()
     {
-        currentThetaLevel = (currentThetaLevel + 1) % (numberOfThetaLevel + 1);
-        targetTheta = Mathf.Lerp(minTheta, maxTheta, currentThetaLevel / numberOfThetaLevel);
+        int closestSuperiorLevel = (int)(TargetTheta * numberOfThetaLevel)+1;   //The closest greater step the target is from
+        float angleClosestLevel = (float)closestSuperiorLevel / numberOfThetaLevel; //The angle corresponding to the step
 
-        //StartLerpCoroutine();
+
+        if (Mathf.Abs(angleClosestLevel - targetTheta) < .3f / numberOfThetaLevel) //If the difference between the step angle and our current angle is greater than 1/3 of a step
+        {
+            TargetTheta = angleClosestLevel + 1f / numberOfThetaLevel;   // we consider targetTheta to be on the step and go to the next
+        }
+        else
+        {
+            TargetTheta = angleClosestLevel;    //Else we go to the closest step
+        }
     }
 
+    //Turn the camera clockwise
     private void MoveToPreviousThetaLevel()
     {
-        currentThetaLevel = ((currentThetaLevel - 1) < 0) ? numberOfThetaLevel : currentThetaLevel - 1;
-        targetTheta = Mathf.Lerp(minTheta, maxTheta, currentThetaLevel / numberOfThetaLevel);
+        int closestInferiorLevel = (int)(TargetTheta * numberOfThetaLevel);     //The closest smaller step the target is from
+        float angleClosestLevel = (float)closestInferiorLevel / numberOfThetaLevel; //The angle corresponding to the step
 
-        //StartLerpCoroutine();
+        if (Mathf.Abs(angleClosestLevel - TargetTheta) <.3f/numberOfThetaLevel) //If the difference between the step angle and our current angle is greater than 1/3 of a step
+        {
+            TargetTheta = angleClosestLevel - 1f / numberOfThetaLevel;   // we consider targetTheta to be on the step and go to the previous one
+        }
+        else
+        {
+            TargetTheta = angleClosestLevel;    //Else we go to the closest step
+        }
     }
-
-    //private void StartLerpCoroutine()
-    //{
-    //    if (lerpingCoroutine != null)
-    //        StopCoroutine(lerpingCoroutine);
-    //    lerpingCoroutine = StartCoroutine(LerpPosition());
-    //}
-
-    //private IEnumerator LerpPosition()
-    //{
-    //    float beforeLerpRho = currentRho,
-    //        beforeLerpTheta = currentTheta,
-    //        beforeLerpPhi = currentPhi;
-
-    //    float startTime = Time.time;
-           
-    //    while(Time.time < startTime+lerpingTime)
-    //    {
-    //        float interval = (Time.time - startTime) / lerpingTime;
-
-    //        currentRho = Mathf.Lerp(beforeLerpRho, targetRho, interval);
-    //        currentTheta = Mathf.Lerp(beforeLerpTheta, targetTheta, interval);
-    //        currentPhi = Mathf.Lerp(beforeLerpPhi, targetPhi, interval);
-
-    //        yield return null;
-    //    }
-
-    //    lerpingCoroutine = null;
-    //}
 
     private Vector3 SphericalToCartesianPos(float phi, float theta, float rho)
     {
         Vector3 cartesianPos;
-        float radPhi = phi * Mathf.Deg2Rad;
+        float radPhi = phi * Mathf.Deg2Rad;         //The radians value of the angles
         float radTheta = theta * Mathf.Deg2Rad;
 
-        float a = rho * Mathf.Cos(radPhi); //??
+        float a = rho * Mathf.Cos(radPhi); 
 
         cartesianPos.x = a * Mathf.Cos(radTheta);
         cartesianPos.y = rho * Mathf.Sin(radPhi);
         cartesianPos.z = a * Mathf.Sin(radTheta);
 
         return cartesianPos;
+    }
+
+    private float GetInterpolateTheta()
+    {
+        return Mathf.Lerp(minTheta, maxTheta, currentTheta);
+    }
+
+    private float GetInterpolateRho()
+    {
+        return Mathf.Lerp(minRho, maxRho, currentRho);
+    }
+
+    private float GetInterpolatePhi()
+    {
+        return Mathf.Lerp(minPhi, maxPhi, currentPhi);
     }
 }
